6e808859c9517885da0b042481a36b9e57dd56cc
bhosalepranil16 <bhosalepranil16@gmail.com>
2023-01-21 21:34:48 +0530

    static url added

--- AssignmentsHelpUI/settings.py
+++ AssignmentsHelpUI/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['dolphin-app-gvtcn.ondigitalocean.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'dolphin-app-gvtcn.ondigitalocean.app']
 
 
 # Application definition
