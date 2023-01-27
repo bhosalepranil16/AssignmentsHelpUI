0168287be9e5c532fccda5b7a4835c17e4dbaf52
bhosalepranil16 <bhosalepranil16@gmail.com>
2023-01-28 00:51:43 +0530

    removed tags model

--- Assignment/admin.py
+++ Assignment/admin.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
 
-from .models import AssignmentModel, TagModel
+from .models import AssignmentModel
 
 
 # Register your models here.
-
-class TagAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    list_filter = ('name',)
 
 
 class AssignmentAdmin(admin.ModelAdmin):
@@ -17,4 +13,3 @@ class AssignmentAdmin(admin.ModelAdmin):
 
 
 admin.site.register(AssignmentModel, AssignmentAdmin)
-admin.site.register(TagModel, TagAdmin)
--- Assignment/models.py
+++ Assignment/models.py
@@ -6,16 +6,6 @@ from Subject.models import SubjectModel
 
 # Create your models here.
 
-class TagModel(models.Model):
-    name = models.CharField(max_length=50)
-
-    class Meta:
-        verbose_name = 'Tag'
-        verbose_name_plural = 'Tags'
-
-    def __str__(self):
-        return self.name
-
 
 class AssignmentModel(models.Model):
     name = models.CharField(max_length=100)
@@ -25,7 +15,6 @@ class AssignmentModel(models.Model):
     codes = models.CharField(max_length=200)
     outputs = ArrayField(models.CharField(max_length=200))
     subject = models.ForeignKey(SubjectModel, related_name='assignments', on_delete=models.CASCADE)
-    tags = models.ManyToManyField(TagModel)
 
     class Meta:
         verbose_name = 'Assignment'
