b820cc50fb6307b1af1cecdd3d5c87b9d2ae3fd4
bhosalepranil16 <bhosalepranil16@gmail.com>
2023-02-11 19:43:36 +0530

    URL Update

--- Home/urls.py
+++ Home/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('about-us/', AboutUsView.as_view(), name='about_us_view'),
     path('privacy-policy/', PrivacyPolicyView.as_view(), name='privacy_policy_view'),
     path('terms-and-conditions/', TermsAndConditionView.as_view(), name='terms_and_condition_view'),
-    path('google2f18d2468a4ed341', GoogleSearchConsoleView.as_view(), name='google_search_console_view')
+    path('google2f18d2468a4ed341.html', GoogleSearchConsoleView.as_view(), name='google_search_console_view')
 ]
