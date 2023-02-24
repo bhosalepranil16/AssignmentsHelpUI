ec404f2a92efe908c4f2f70e2a455c833c625b0d
bhosalepranil16 <bhosalepranil16@gmail.com>
2023-02-24 20:55:40 +0530

    Minor Change

--- Subject/views.py
+++ Subject/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 
-from .models import SubjectModel, DocumentModel
+from .models import SubjectModel
 
 
 # Create your views here.
